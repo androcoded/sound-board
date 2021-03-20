654f2ca3d28aaaf32a6f168847ea316fa22455a2
ändrocoded <androcoded@gmail.com>
2021-03-20 13:06:34 +0600

    Final project

---
+++ script.js
@@ -0,0 +1,22 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add("btn");
+  btn.innerHTML = sound;
+  btn.addEventListener("click", () => {
+    stopSong();
+    document.getElementById(sound).play();
+    
+  });
+
+  document.getElementById('buttons').appendChild(btn);
+  console.log(buttons)
+});
+
+function stopSong() {
+  sounds.forEach((sound) => {
+    document.getElementById(sound).pause();
+    document.getElementById(sound).currentTime = 0;
+  });
+}
